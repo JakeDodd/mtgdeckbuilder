f4afe0efc28b17b69825fce213069aa56d219c9a
Jacob Dodd <doddj@duq.edu>
2026-03-26 20:07:45 -0400

    refactored random card page according to back end changes

--- react/src/components/RandomCardPage.jsx
+++ react/src/components/RandomCardPage.jsx
@@ -8,11 +8,24 @@ function RandomCardPage() {
   const [imageUrl, setImageUrl] = useState("");
   const [backImageUrl, setBackImageUrl] = useState("");
 
+  const getImageUrl = (card) => {
+    const prints = card.Prints
+    const english = prints.filter(p => { p.Lang === 'en' })
+    const highres = english.filter(p => p.HiresImage)
+    var print
+    if (highres.length > 0) {
+      print = highres[0]
+    } else if (english.length > 0) {
+      print = english[0]
+    } else {
+      print = prints[0]
+    }
+    return print.CardFaces?.length > 0 ? print.CardFaces[0].NormalUri : print.NormalUri
+  }
+
   const getCard = (card) => {
     setCard(card);
-    setImageUrl(
-      card.CardFaces?.length > 0 ? card.CardFaces[0].NormalUri : card.NormalUri,
-    );
+    setImageUrl(getImageUrl(card));
     setBackImageUrl(
       "https://backs.scryfall.io/normal/2/2/222b7a3b-2321-4d4c-af19-19338b134971.jpg?1677416389",
     );
